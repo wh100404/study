5596d1d5a09562ad516e928f2316388225ebc870
SQ-0506 <240022432@qq.com>
2021-02-04 23:46:51 +0800

    Day05

---
+++ src/main/java/com/sq/Day5_01.java
@@ -0,0 +1,18 @@
+package com.sq;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Day5_01 {
+    public static void main(String[] args) {
+        JFrame jframe =new JFrame("动态数组");
+        Container container=jframe.getContentPane();
+        container.setBackground(Color.CYAN);
+        jframe.setBounds(60,100,500,500);
+        jframe.setResizable(true);
+        jframe.setVisible(true);
+        jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+
+}
