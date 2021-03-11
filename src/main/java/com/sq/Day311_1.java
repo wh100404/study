8f49c2e585b42e48e5aa00be211dcd28a9215aca
SQ-0506 <240022432@qq.com>
2021-03-11 20:47:49 +0800

    Day311

---
+++ src/main/java/com/sq/Day311_1.java
@@ -0,0 +1,12 @@
+package com.sq;
+
+import java.util.Scanner;
+
+public class Day311_1 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String a=sc.nextLine();
+        System.out.println("请输入身份证号：");
+        System.out.println("原来你的身份证号是"+a.length()+"位");
+    }
+}
