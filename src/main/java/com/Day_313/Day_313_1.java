12d71ea29446e5403aad218437a5cb4263155c8a
SQ-0506 <240022432@qq.com>
2021-03-13 23:09:49 +0800

    先实现一部分  剩下的还没想起来 明天接着实现  hahahahha

---
+++ src/main/java/com/Day_313/Day_313_1.java
@@ -0,0 +1,48 @@
+package com.Day_313;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+public class Day_313_1 extends JFrame {
+    public Day_313_1(){
+        setBounds(200,200,800,500);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        Container c=getContentPane();
+        setLayout(null);
+        JTextArea jt=new JTextArea();
+        jt.setColumns(100);
+        jt.setBounds(0,80,600,350);
+        c.add(jt);
+        JTextField js=new JTextField();
+        js.setBounds(200,30,300,30);
+        c.add(js);
+
+        c.add(new JLabel("学生姓名"));
+        JButton btn1=new JButton("添加学生");
+        btn1.setBounds(650,100,150,30);
+        JButton btn2=new JButton("删除学生");
+        btn2.setBounds(650,200,150,30);
+        btn1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                jt.append(js.getText());
+            }
+        });
+        btn2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+            }
+        });
+        c.add(btn1);
+        c.add(btn2);
+
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new Day_313_1();
+    }
+}
