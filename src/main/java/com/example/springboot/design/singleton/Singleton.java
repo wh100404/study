fcc64b79c073f676b66dfd2223f9e5f7ba0fcc4b
wanghao <wh_199404@163.com>
2021-03-23 21:28:31 +0800

    单例模式

---
+++ src/main/java/com/example/springboot/design/singleton/Singleton.java
@@ -0,0 +1,17 @@
+package com.example.springboot.design.singleton;
+
+public class Singleton {
+
+    private static Singleton singleton;
+
+    private Singleton() {
+
+    }
+
+    public static Singleton getInstance() {
+        if (singleton == null) {
+            singleton = new Singleton();
+        }
+        return singleton;
+    }
+}
