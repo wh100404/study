a0e8fed279381751fb54649d1ff853cc89f06ce5
wanghao <wh_199404@163.com>
2021-03-29 21:02:26 +0800

    适配器模式-2

--- src/main/java/com/ctks/Day2_5/PingPang.java
+++ src/main/java/com/ctks/Day2_5/PingPang.java
@@ -2,19 +2,19 @@ package com.ctks.Day2_5;
 
 public class PingPang {
     public static void main(String[] args) {
-        String[] arr={"a","b","c"};
-        String[] arr1={"x","y","z"};
-        for (int i = 0; i <arr.length ; i++) {
-            for (int j = 0; j <arr1.length ; j++) {
-                  if(arr[i]=="a"&&(arr1[j]=="x"||arr1[j]=="y")){
-                       continue;
-                  }else if(arr[i]=="c"&&(arr1[j]=="x"||arr1[j]=="z")){
-                       continue;
-                  }else if(arr[i]=="b"&&(arr1[j]=="y"||arr1[j]=="z")){
-                      continue;
-                }else{
-                      System.out.println("与"+arr[i]+"比赛的是"+arr1[j]);
-                  }
+        String[] arr = {"a", "b", "c"};
+        String[] arr1 = {"x", "y", "z"};
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr1.length; j++) {
+                if (arr[i] == "a" && (arr1[j] == "x" || arr1[j] == "y")) {
+                    continue;
+                } else if (arr[i] == "c" && (arr1[j] == "x" || arr1[j] == "z")) {
+                    continue;
+                } else if (arr[i] == "b" && (arr1[j] == "y" || arr1[j] == "z")) {
+                    continue;
+                } else {
+                    System.out.println("与" + arr[i] + "比赛的是" + arr1[j]);
+                }
             }
         }
     }
---
+++ src/main/java/com/example/springboot/design/adaptor/HomeTheaterFacade.java
@@ -0,0 +1,61 @@
+package com.example.springboot.design.adaptor;
+
+import headfirst.facade.hometheater.*;
+
+public class HomeTheaterFacade {
+    Amplifier amp;
+    Tuner tuner;
+    DvdPlayer dvdPlayer;
+    CdPlayer cdPlayer;
+    Projector projector;
+    TheaterLights lights;
+    Screen screen;
+    PopcornPopper popper;
+
+    public HomeTheaterFacade(Amplifier amp,
+                             Tuner tuner,
+                             DvdPlayer dvdPlayer,
+                             CdPlayer cdPlayer,
+                             Projector projector,
+                             TheaterLights lights,
+                             Screen screen,
+                             PopcornPopper popper) {
+        this.amp = amp;
+        this.tuner = tuner;
+        this.dvdPlayer = dvdPlayer;
+        this.cdPlayer = cdPlayer;
+        this.projector = projector;
+        this.lights = lights;
+        this.screen = screen;
+        this.popper = popper;
+    }
+
+    public void watchMovie(String movie) {
+        System.out.println("Get ready to watch a movie.....");
+        popper.on();
+        popper.pop();
+        lights.dim(10);
+        screen.down();
+        projector.on();
+        projector.wideScreenMode();
+        amp.on();
+        amp.setDvd(dvdPlayer);
+        amp.setSurroundSound();
+        amp.setVolume(5);
+        dvdPlayer.on();
+        dvdPlayer.play(movie);
+        amp.setCd(cdPlayer);
+    }
+
+    public void endMovie() {
+        System.out.println("Shutting movie theater down...");
+        popper.off();
+        lights.on();
+        screen.up();
+        projector.off();
+        amp.off();
+        dvdPlayer.stop();
+        dvdPlayer.eject();
+        dvdPlayer.off();
+    }
+}
